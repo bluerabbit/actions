d944b8d4a7bc753995b6adf79b800bca67214ba5
Akira Kusumoto <akirakusumo10@gmail.com>
2020-01-30 10:53:55 +0900

    :sparkles: Add Github Actions feature-branch

---
+++ bin/feature-branch/README.md
@@ -0,0 +1,20 @@
+# feature-branch
+
+## Usage
+
+```yaml
+name: feature-branch
+on:
+  pull_request:
+    types: [opened]
+
+jobs:
+  feature_branch:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: bluerabbit/actions/bin/release-version@master
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          PULL_REQUEST_NUMBER: ${{ github.event.number }}
+```
---
+++ bin/feature-branch/lib/main.rb
@@ -0,0 +1,14 @@
+$LOAD_PATH << File.dirname(__FILE__)
+
+require "octokit"
+
+client     = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+current_pr = client.pull_request(ENV["GITHUB_REPOSITORY"], ENV["PULL_REQUEST_NUMBER"])
+
+prs = client.pull_requests(ENV["GITHUB_REPOSITORY"], query: { ref: current_pr.base.ref })
+
+if parent_pr = prs.detect {|pr| pr.head.ref == current_pr.base.ref }
+  client.update_pull_request(ENV["GITHUB_REPOSITORY"],
+                             ENV["PULL_REQUEST_NUMBER"],
+                             body: "#{current_pr.body}\n\n## Base Branch\n\n- ##{parent_pr.number}")
+end
